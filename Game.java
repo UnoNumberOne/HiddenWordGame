77b7838440b66a641126b46fcb9a1987ce8f9a09
UnoNumberOne <urebustes@student-bmhs-la.org>
2023-01-06 12:44:00 -0800

    FINISHED PRODUCT

--- Game.java
+++ Game.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Game
 {
     private String hidden;
@@ -11,7 +12,37 @@ public class Game
     }
     public void run()
     {
-       System.out.println("Hello World");
-    }
+        Scanner kb = new Scanner(System.in);
 
+        System.out.println("Please enter a word that someone will guess");
+        hidden = kb.nextLine();
+
+        int hiddenLength = hidden.length();
+
+        //write loops here
+        while(!hidden.equals(guess))
+        {
+            hint = "";
+            System.out.println("Enter a " + hiddenLength + " letter word");
+            guess = kb.nextLine();
+
+            for(int i = 0; i < hidden.length(); i++)
+            {
+                if(hidden.substring(i, i+1).equals(guess.substring(i, i+1)))
+                {
+                     hint += guess.substring(i, i+1);
+                }
+                else if(hidden.indexOf(guess.substring(i, i+1))!= -1)
+                {
+                     hint += "+";
+                }
+                else
+                {
+                    hint += "*";
+                }
+            }
+            System.out.println(hint);
+        }
+        System.out.println("You Guessed the correct word");
+    }
 }
